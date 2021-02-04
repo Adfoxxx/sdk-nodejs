cff0e6e8e9021d1e425470e1372ea2a5b223933f
ADFOX <info@admart.ua>
2021-02-04 16:11:29 +0200

    зззз

---
+++ javascript.js
@@ -0,0 +1,91 @@
+// let a= prompt("Сколько тебе лет",100);
+
+// alert(`Тебе ${a} лет`)
+
+
+// let b=function (showmesage) {
+
+//     alert('Privet')
+// } 
+// b()
+
+// let sayHi = b=> b*2;
+
+// alert(sayHi(222));
+
+// let vito=prompt('Привет', '')
+
+// let vv=vito==18?alert('East'):alert('not')
+
+
+// let message;
+
+//  (login == 'Сотрудник') ?
+//   message = 'Привет':
+// (login == 'Директор') ?
+//   message = 'Здравствуйте':
+//  (login == '') ?
+//   message = 'Нет логина';:
+
+//   message = '';
+
+
+//   let form1=prompt('Privet','')
+
+//   if (form1=='123'){
+//     alert('Privet12')
+
+//   } else if (form1=''|| form1==null) 
+//   {
+//     alert('Poka')
+//   }
+//   let value = prompt('Введите число', '');
+
+// if (value > 0) {
+//   alert( 1 );
+// } else if (value < 0) {
+//   alert( -1 );
+// } else {
+//   alert( 0 );
+// }
+// let height = 1;
+// let width = null;
+// let area = (height ?? 100) * (width ?? 50);
+// console.log(area);
+
+// let i = 0;
+
+// for (i = 0; i < 3; i++) { // используем существующую переменную
+//   alert(i); // 0, 1, 2
+// }
+
+// alert(i); 
+// console.log(i)
+
+
+
+// outer: for (let i = 0; i < 3; i++) {
+
+//   for (let j = 0; j < 3; j++) {
+
+//     let input = prompt(`Значение на координатах (${i},${j})`, '');
+
+//     // если пустая строка или Отмена, то выйти из обоих циклов
+//     if (!input) break outer; // (*)
+// alert('Все хорошо')
+//     // сделать что-нибудь со значениями...
+//   }
+// }
+
+// alert('Готово!');
+// document.body.style.background = 'red'; // сделать фон красным
+
+// setTimeout(() => document.body.style.background = '', 6000); // вернуть назад
+
+// function showMessage(from, text) {
+//     // Если значение text ложно, тогда присвоить параметру text значение по умолчанию
+//     text = text || 'текст не добавлен';
+//     ...
+//   }
+//   console.log(showMessage(1,1))
+ppp.hidden = true
